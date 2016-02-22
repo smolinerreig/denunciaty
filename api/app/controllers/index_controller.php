b0d1b368e4ae634fdc33a8039fc9111eecc36330
niptim <s.moliner.reig@gmail.com>
2016-02-22 14:33:30 +0100

    puliendo estilos

--- api/app/controllers/index_controller.php
+++ api/app/controllers/index_controller.php
@@ -2,7 +2,7 @@
 Load::model('puntos_acceso');
 /**
  * Controller por defecto si no se usa el routes
- * 
+ *
  */
 class IndexController extends AppController
 {
@@ -10,7 +10,11 @@ public function before_filter() {
 		View::template ( "json" );
 		View::select ( null );
 	}
-    
+
+    /*
+    *Devuelve todo los puntos de acceso WIFI.
+    *Ejemplo: http://denunciaty/api/index/puntos
+    */
     public function puntos(){
         $puntos=new PuntosAcceso();
         $this->data=$puntos->getTodos();
